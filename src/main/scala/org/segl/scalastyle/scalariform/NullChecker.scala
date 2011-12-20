e5b581e4b015393ac39bbf7b60d3bf198bb281e0
Matthew Farwell <matthew@farwell.co.uk>
2011-12-20 23:57:59 +0100

    Changes due to scalastyle

--- src/main/scala/org/segl/scalastyle/scalariform/NullChecker.scala
+++ src/main/scala/org/segl/scalastyle/scalariform/NullChecker.scala
@@ -8,7 +8,7 @@ import org.segl.scalastyle._
 
 class NullChecker extends ScalariformChecker {
   val errorKey = "null"
-    
+
   def verify(ast: CompilationUnit): List[ScalastyleError] = {
     val it = for (
       List(left, right) <- ast.tokens.sliding(2);
