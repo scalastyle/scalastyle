bb087506fc07b54d028709f5638105ff53f24522
Matthew Farwell <matthew@farwell.co.uk>
2012-03-08 00:06:15 +0100

    Added licence text to files

--- src/main/scala/org/segl/scalastyle/ScalastyleConfiguration.scala
+++ src/main/scala/org/segl/scalastyle/ScalastyleConfiguration.scala
@@ -1,3 +1,19 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.segl.scalastyle
 
 import scala.xml.XML
@@ -27,11 +43,13 @@ object ScalastyleConfiguration {
   }
 
   def toCheck(node: Node): ConfigurationChecker = {
-    val className = node.attribute("class").head.text
-    val level = Level(node.attribute("level").head.text)
+    val className = node.attribute("class").get.text
+    val level = Level(node.attribute("level").get.text)
 
     ConfigurationChecker(className, level, (node \\ "parameters" \\ "parameter").map(e => {
-      (e.attribute("name").head.text -> e.attribute("value").head.text)
+      val attributeValue = e.attribute("value")
+      val value = if (attributeValue.isDefined) attributeValue.get.text else e.text
+      (e.attribute("name").head.text -> value)
     }).toMap)
   }
 
--- src/test/scala/org/segl/scalastyle/ScalastyleConfigurationTest.scala
+++ src/test/scala/org/segl/scalastyle/ScalastyleConfigurationTest.scala
@@ -14,24 +14,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.segl.scalastyle.scalariform
+package org.segl.scalastyle
 
-import scalariform.parser.CompilationUnit
-import _root_.scalariform.lexer.Tokens._
-import _root_.scalariform.lexer.Token
-import _root_.scalariform.parser._
-import org.segl.scalastyle.ScalariformChecker
-import org.segl.scalastyle._
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import file.HeaderMatchesChecker
 
-class EqualsHashCodeChecker extends AbstractMethodChecker {
-  val errorKey = "equals.hash.code"
-
-  def matches(t: BaseClazz[AstNode]): Boolean = {
-    val hc = t.subs.exists(matchFunDefOrDcl(_, isHashCode))
-    val eq = t.subs.exists(matchFunDefOrDcl(_, isEqualsObject))
-
-    (hc && !eq) || (!hc && eq)
+class ScalastyleConfigurationTest extends AssertionsForJUnit {
+  // just check we can read it
+  @Test def readXml(): Unit = {
+    val config = ScalastyleConfiguration.readFromXml("lib/scalastyle_config.xml")
   }
-
-  private def isHashCode(t: FunDefOrDcl): Boolean = methodMatch("hashCode", noParameter() _)(t)
 }
