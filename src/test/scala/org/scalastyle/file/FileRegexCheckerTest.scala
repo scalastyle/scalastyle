418f861654e1b7a43440a79dc071f3ecfd0495e0
Graham Berks <graham@fatlazycat.com>
2012-04-08 17:39:37 +0100

    regex checkers for line and file

---
+++ src/test/scala/org/scalastyle/file/FileRegexCheckerTest.scala
@@ -0,0 +1,59 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.file
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Test
+
+class FileRegexCheckerTest extends AssertionsForJUnit with CheckerTest {
+  val key = "multi.line.regex"
+  val classUnderTest = classOf[FileRegexChecker]
+
+  val source = """
+  package foobar
+
+
+class foobar {
+
+  def aMethod: String = {
+    println("SHOULD NOT BE HERE")
+    "TEST STRING"
+  }
+
+}
+"""
+
+  @Test
+  def testMultiLineCheck() {
+    assertErrors(List(fileError()), source, Map("regex" -> "(?m)^\\s*\\}\\n^\\s*\\n^}"))
+  }
+
+  @Test
+  def testMultipleMatchesReportsOneFileError() {
+    assertErrors(List(fileError()), source, Map("regex" -> "\\}"))
+  }
+
+  @Test
+  def testDoubleBlankLinesCheck() {
+    assertErrors(List(fileError()), source, Map("regex" -> "(?m)^\\s*\\n^\\s*\\n"))
+  }
+
+  @Test
+  def testCannotFindMatch() {
+    assertErrors(List(), source, Map("regex" -> "^SHOULD$"))
+  }
+}
---
+++ src/test/scala/org/scalastyle/file/LineRegexCheckerTest.scala
@@ -0,0 +1,60 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.file
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Test
+
+// scalastyle:off magic.number
+
+class LineRegexCheckerTest extends AssertionsForJUnit with CheckerTest {
+  val key = "regex"
+  val classUnderTest = classOf[LineRegexChecker]
+
+  val source = """
+  package foobar
+
+class foobar {
+
+  def aMethod: String = {
+    println("SHOULD NOT BE HERE")
+    "TEST STRING"
+  }
+
+}
+"""
+
+  @Test
+  def testSingleMatchCheck() {
+    assertErrors(List(lineError(7)), source, Map("regex" -> "println"))
+  }
+
+  @Test
+  def testSingleMatchWithBoundsCheck() {
+    assertErrors(List(lineError(11)), source, Map("regex" -> "^\\}$"))
+  }
+
+  @Test
+  def testMultipleMatchCheck() {
+    assertErrors(List(lineError(9), lineError(11)), source, Map("regex" -> "\\}"))
+  }
+
+  @Test
+  def testCannotFindMatch() {
+    assertErrors(List(), source, Map("regex" -> "^SHOULD$"))
+  }
+}
