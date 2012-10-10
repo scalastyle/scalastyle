05724db7a81d3815f2803000be93e80682b8cb5a
Matthew Farwell <matthew@farwell.co.uk>
2012-10-10 17:21:01 +0200

    Added DeprecatedJavaChecker

---
+++ src/main/scala/org/scalastyle/scalariform/DeprecatedJavaChecker.scala
@@ -0,0 +1,55 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.scalariform;
+
+import java.lang.reflect.Constructor
+import scalariform.parser.CompilationUnit
+import _root_.scalariform.lexer.Tokens._
+import org.scalastyle.ScalariformChecker
+import org.scalastyle._
+import org.scalastyle.FileSpec
+import _root_.scalariform.lexer.Token
+import _root_.scalariform.parser.Refinement
+import  _root_.scalariform.parser.{Annotation => ParserAnnotation}
+
+class DeprecatedJavaChecker extends ScalariformChecker {
+  val errorKey = "deprecated.java"
+  import VisitorHelper._
+  val deprecatedTokens = List("Deprecated", "java.lang.Deprecated")
+
+  case class Position(position: Option[Int])
+
+  final def verify(ast: CompilationUnit): List[ScalastyleError] = {
+    val it = for (
+      f <- localvisit(ast.immediateChildren(0))
+    ) yield {
+      PositionError(f.position.get)
+    }
+
+    it.toList
+  }
+
+  private def isDeprecated(t: ParserAnnotation) = {
+    val text = t.annotationType.tokens.foldLeft("")((x, y) => x + y.text)
+    t.annotationType.tokens.size > 0 && deprecatedTokens.contains(text)
+  }
+
+  private def localvisit(ast: Any): List[Position] = ast match {
+    case t: ParserAnnotation if (isDeprecated(t)) => List(Position(Some(t.firstToken.offset)))
+    case t: Any => visit(t, localvisit)
+  }
+}
---
+++ src/test/scala/org/scalastyle/scalariform/DeprecatedJavaCheckerTest.scala
@@ -0,0 +1,59 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.scalariform
+
+import org.scalastyle.file.CheckerTest
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.scalastyle.Checker
+import org.scalastyle.StyleError
+import java.util.Set
+import org.junit.Before
+import org.junit.Test
+
+// scalastyle:off magic.number
+
+class DeprecatedJavaCheckerTest extends AssertionsForJUnit with CheckerTest {
+  val key = "deprecated.java"
+  val classUnderTest = classOf[DeprecatedJavaChecker]
+
+  @Test def testClassOK() {
+    val source = """
+package foobar
+
+class OK {
+  @Deprecated
+  def noparameters(): Any = null
+
+  @Deprecated("foobar")
+  def parameters(): Any = null
+
+  @java.lang.Deprecated
+  def noparametersFull(): Any = null
+
+  @java.lang.Deprecated("foobar")
+  def parametersFull(): Any = null
+
+  @deprecated
+  def ok(): Any = null
+}
+""";
+
+    assertErrors(List(columnError(5, 2), columnError(8, 2), columnError(11, 2), columnError(14, 2)), source)
+  }
+}
