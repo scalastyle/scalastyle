237e4027c2a1c767f6c93d95a91eaeb5e80f92ab
Matthew Farwell <matthew@farwell.co.uk>
2012-10-17 20:24:29 +0200

    Added LowercasePattermMatchChecker

---
+++ src/main/scala/org/scalastyle/scalariform/LowercasePatternMatchChecker.scala
@@ -0,0 +1,60 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.scalariform;
+
+import org.scalastyle.FileError
+import org.scalastyle.ScalariformChecker
+import org.scalastyle.ScalastyleError
+import org.scalastyle.PositionError
+
+import scalariform.parser.CompilationUnit
+import scalariform.parser.TmplDef
+import scalariform.parser.CasePattern
+import scalariform.parser.Expr
+import scalariform.parser.GeneralTokens
+import scalariform.lexer.Token
+import scalariform.lexer.Tokens.VARID
+
+class LowercasePatternMatchChecker extends ScalariformChecker {
+  val errorKey = "lowercase.pattern.match"
+  import VisitorHelper.visit
+
+  case class Position(position: Option[Int])
+
+  final def verify(ast: CompilationUnit): List[ScalastyleError] = {
+    val it = for (
+      f <- localvisit(ast.immediateChildren(0));
+      if (matches(f))
+    ) yield {
+      PositionError(f.pattern.firstToken.offset)
+    }
+
+    it.toList
+  }
+
+  private def matches(t: CasePattern) = {
+    t.pattern.tokens match {
+      case List(t: Token) => (t.tokenType == VARID && t.text.length() > 0 && t.text(0).isLower)
+      case _ => false
+    }
+  }
+
+  private def localvisit(ast: Any): List[CasePattern] = ast match {
+    case t: CasePattern => List(t) ::: localvisit(t.pattern) ::: localvisit(t.guardOption)
+    case t: Any => visit(t, localvisit)
+  }
+}
---
+++ src/test/scala/org/scalastyle/scalariform/LowercasePatternMatchCheckerTest.scala
@@ -0,0 +1,72 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.scalariform
+
+import org.scalastyle.file.CheckerTest
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.scalastyle.Checker
+import org.scalastyle.StyleError
+import java.util.Set
+import org.junit.Before
+import org.junit.Test
+
+// scalastyle:off magic.number
+
+class LowercasePatternMatchCheckerTest extends AssertionsForJUnit with CheckerTest {
+  val key = "lowercase.pattern.match"
+  val classUnderTest = classOf[LowercasePatternMatchChecker]
+
+  @Test def testOK() {
+    val source = """
+package foobar
+
+class F1 {
+  val lc = "foobar"
+
+  def fn(a: Any) = a match {
+    case "barbar" => "barbar"
+    case s: Int => "int"
+    case List(x, y) => "list"
+    case `lc` => "lc"
+  }
+}
+""";
+
+    assertErrors(List(), source)
+  }
+
+  @Test def testKO() {
+    val source = """
+package foobar
+
+class F1 {
+  val lc = "foobar"
+
+  def fn(a: Any) = a match {
+    case "barbar" => "barbar"
+    case s: Int => "int"
+    case List(x, y) => "list"
+    case lc => "lc"
+  }
+}
+""";
+
+    assertErrors(List(columnError(11, 9)), source)
+  }
+}
