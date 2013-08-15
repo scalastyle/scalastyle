57a968d9407044f93406f90f25dc2da64a269326
shoshi_tamaki <shoshi_tamaki@dwango.co.jp>
2013-08-15 20:55:10 +0900

    added SimpleExprWithBraceChecker

---
+++ src/main/scala/org/scalastyle/scalariform/SimpleExprWithBraceChecker.scala
@@ -0,0 +1,155 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.scalariform
+
+import _root_.scalariform.parser._
+import _root_.scalariform.lexer._
+import _root_.scalariform.lexer.Tokens._
+import org.scalastyle.{CombinedAst, CombinedChecker, ScalastyleError, PositionError, Lines}
+import VisitorHelper.visit
+
+
+/**
+ * According to the Effective Scala "http://twitter.github.io/effectivescala/index.html#Formatting-Braces"
+ * Braces are used to create compound expressions, using braces with simple statement is hard to read.
+ * Effective Scala suggests avoid using braces with simple expression.
+ *
+ * this checker detects the simple expression where using braces like below,
+ *
+ * def foo() = {
+ *  bar()
+ * }
+ *
+ * the method above contains only one expression "bar()", so we can remove braces
+ *
+ * def foo() = bar()
+ *
+ * Configuration.
+ * 1, To configure token to check, give parameter to checker in XML. like this,
+ *
+ * <parameters>
+ *  <parameter name="targetTokens">def,for,while</parameter>
+ * </parameter>
+ *
+ * To specify multiple tokens, write all expression name and separate them with ","
+ * available tokens are def,val,var,if,else,for,while,do,case
+ */
+
+class SimpleExprWithBraceChecker extends CombinedChecker {
+  val errorKey = "simple.expression.with.brace"
+
+  val ParamTargetTokens = "targetTokens"
+  val ParamTargetTokensDefValue = ""
+
+  val TargetTokensSeparator = ","
+
+  case class SourceContext(lines: Lines, targetTokens: Set[TokenType])
+
+  override def verify(cAst: CombinedAst): List[ScalastyleError] = {
+    val targetTokensString = getString(ParamTargetTokens, ParamTargetTokensDefValue)
+    val targetTokens = (for (
+      tokenString <- targetTokensString.split(TargetTokensSeparator)
+    ) yield TokenType(tokenString.toUpperCase())).toSet
+
+    val sourceContext = SourceContext(cAst.lines, targetTokens)
+
+    val it = for (Some(a) <- visit(cAst.compilationUnit, localvisit(_, sourceContext))) yield a
+
+    it.toList
+  }
+
+  private def localvisit(ast: Any, sc: SourceContext): List[Option[ScalastyleError]] = ast match {
+    case e: ExprElement => visitExprElement(e, sc)
+    case d: DefOrDcl => visitDefOrDcl(d, sc)
+    case c: CaseClause => visitCaseClause(c, sc)
+    case a: Any => visit(a, localvisit(_, sc))
+  }
+
+  private def checkBlockExprHasSingleExpr(b: BlockExpr): Option[ScalastyleError] = b.caseClausesOrStatSeq match {
+    case Right(s: StatSeq) if s.otherStats.isEmpty => Some(PositionError(b.lbrace.offset))
+    case a: Any => None
+  }
+
+  private def visitCaseClause(c: CaseClause, sc: SourceContext): List[Option[ScalastyleError]] = {
+    val blockExprOrEmpty = c.statSeq.firstStatOpt match {
+      case Some(x: Expr) => x.contents.take(1)
+      case _ => List()
+    }
+
+    blockExprOrEmpty.flatMap {
+      case x: BlockExpr => checkBlockExprHasSingleExpr(x) :: visit(x, localvisit(_, sc))
+      case _ => visit(c, localvisit(_, sc))
+    }
+  }
+
+  private def visitDefOrDcl(d: DefOrDcl, sc: SourceContext): List[Option[ScalastyleError]] = d match {
+    case f: FunDefOrDcl if sc.targetTokens.contains(DEF) => f.funBodyOpt match {
+      case Some(e: ExprFunBody) => visitExprFunBody(e, sc)
+      case _ => visit(f, localvisit(_, sc))
+    }
+    case p: PatDefOrDcl if sc.targetTokens.contains(p.valOrVarToken.tokenType) => visitPatDefOrDcl(p, sc)
+    case a: Any => visit(a, localvisit(_, sc))
+  }
+
+  private def visitExprFunBody(e: ExprFunBody, sc: SourceContext): List[Option[ScalastyleError]] = e.body.contents match {
+    case (x: BlockExpr) :: xs => checkBlockExprHasSingleExpr(x) :: visit(e.body, localvisit(_, sc))
+    case _ => visit(e.body, localvisit(_, sc))
+  }
+
+  private def visitPatDefOrDcl(p: PatDefOrDcl, sc: SourceContext): List[Option[ScalastyleError]] = p.equalsClauseOption match {
+    case Some((_, e: Expr)) => e.contents match {
+      case (x: BlockExpr) :: xs => checkBlockExprHasSingleExpr(x) :: visit(e, localvisit(_, sc))
+      case _ => visit(e, localvisit(_, sc))
+    }
+    case _ => visit(p, localvisit(_, sc))
+  }
+
+  private def visitExprElement(e: ExprElement, sc: SourceContext): List[Option[ScalastyleError]] = e match {
+    case f: ForExpr => visitExpr(FOR, f.body, sc)
+    case d: DoExpr => visitExpr(DO, d.body, sc)
+    case w: WhileExpr => visitExpr(WHILE, w.body, sc)
+    case i: IfExpr => visitIfExpr(i, sc)
+    case _ => visit(e, localvisit(_, sc))
+  }
+
+  private def visitExpr(t: TokenType, e: Expr, sc: SourceContext): List[Option[ScalastyleError]] = e.contents match {
+    case (x: BlockExpr) :: xs if sc.targetTokens.contains(t) => checkBlockExprHasSingleExpr(x) :: visit(e, localvisit(_, sc))
+    case _ => visit(e, localvisit(_, sc))
+  }
+
+  private def visitIfExpr(i: IfExpr, sc: SourceContext): List[Option[ScalastyleError]] = {
+    val ifExprErrors = i.body.contents match {
+      case (x: BlockExpr) :: xs if sc.targetTokens.contains(IF) =>
+        checkBlockExprHasSingleExpr(x) :: visit(i.body, localvisit(_, sc))
+      case _ => visit(i.body, localvisit(_, sc))
+    }
+
+    val elseClauseErrors = i.elseClause match {
+      case Some(x) => visitElseOrElseif(x, sc)
+      case _ => List()
+    }
+
+    ifExprErrors ::: elseClauseErrors
+  }
+
+  private def visitElseOrElseif(e: ElseClause, sc: SourceContext): List[Option[ScalastyleError]] = e.elseBody.contents match {
+    case (x: IfExpr) :: xs => visitIfExpr(x, sc) ::: visit(xs, localvisit(_, sc))
+    case (x: BlockExpr) :: xs if sc.targetTokens.contains(ELSE) =>
+      checkBlockExprHasSingleExpr(x) :: visit(e.elseBody, localvisit(_, sc))
+    case a: Any => visit(a, localvisit(_, sc))
+  }
+}
---
+++ src/test/scala/org/scalastyle/scalariform/SimpleExprWithBraceCheckerTest.scala
@@ -0,0 +1,326 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// scalastyle:off magic.number
+
+package org.scalastyle.scalariform
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.scalastyle.file.CheckerTest
+import org.junit.Test
+
+
+class SimpleExprWithBraceCheckerTest extends AssertionsForJUnit with CheckerTest {
+  val key = "simple.expression.with.brace"
+  val classUnderTest = classOf[SimpleExprWithBraceChecker]
+
+  @Test def testValOK(): Unit = {
+    val code = """
+class A{
+  val x = 1
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "val"))
+  }
+
+  @Test def testValWithSingleExprBody(): Unit = {
+    val code = """
+class A{
+  val x = {
+    1
+  }
+}
+""".stripMargin
+    assertErrors(List(columnError(3, 10)), code, Map("TargetTokens" -> "val"))
+  }
+
+  @Test def testValWithMultipleExprBody(): Unit = {
+    val code = """
+class A{
+  val x = {
+    1
+    2
+  }
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "val"))
+  }
+
+  @Test def testVarOK(): Unit = {
+    val code = """
+class A{
+  var x = 1
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "var"))
+  }
+
+  @Test def testVarWithSingleExprBody(): Unit = {
+    val code = """
+class A{
+  var x = {
+   1
+  }
+}
+""".stripMargin
+    assertErrors(List(columnError(3, 10)), code, Map("TargetTokens" -> "var"))
+  }
+
+  @Test def testVarWithMultipleExprBody(): Unit = {
+    val code = """
+class A{
+  var x = {
+   1
+   2
+  }
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "var"))
+  }
+
+  @Test def testDefOK(): Unit = {
+    val code = """
+class A{
+ def a() = 1
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "def"))
+  }
+
+  @Test def testDefWithSingleExprBody(): Unit = {
+    val code = """
+class A{
+ def a() = {
+  1
+ }
+}
+""".stripMargin
+
+    assertErrors(List(columnError(3, 11)), code, Map("TargetTokens" -> "def"))
+  }
+
+  @Test def testDefWithMultipleExprBody(): Unit = {
+    val code = """
+class A{
+ def b() = {
+  foo()
+  bar()
+ }
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "def"))
+  }
+
+  @Test def testForWithSingleExprBody(): Unit = {
+    val code = """
+class A{
+ def a() = {
+  for(b <- List(1,2,3)){
+   b + 1
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(columnError(4, 23)), code, Map("TargetTokens" -> "for"))
+  }
+
+  @Test def testForWithMultipleExprBody(): Unit = {
+    val code = """
+class A{
+ def a() = {
+  for(b <- List(1,2,3)){
+   foo()
+   bar()
+  }
+ }
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "for"))
+  }
+
+  @Test def testForSingleLine(): Unit = {
+    val code = """
+package foobar
+class A{
+ def a() = {
+  // single line for expr
+  for(b <- List(1,2,3)) foo()
+ }
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "for"))
+  }
+
+  @Test def testWhileSingleLineBody(): Unit = {
+    val code = """
+package foobar
+class A{
+ def a() = {
+  while(true){
+   foo()
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(columnError(5, 13)), code, Map("TargetTokens" -> "while"))
+  }
+
+  @Test def testWhileLongBody(): Unit = {
+    val code = """
+package foobar
+class A{
+ def a() = {
+  while(true){
+   foo()
+   bar()
+  }
+ }
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "while"))
+  }
+
+  @Test def testWhileSingleLine(): Unit = {
+    val code = """
+package foobar
+class A{
+ def a() = {
+  while(true) 1
+ }
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "while"))
+  }
+
+  @Test def testIfOK(): Unit = {
+    val code = """
+package foobar
+class A{
+ def a() = {
+  if(true) foo()
+ }
+}
+""".stripMargin
+
+    assertErrors(List(), code, Map("TargetTokens" -> "if"))
+  }
+
+  @Test def testIfWithSingleExprBody(): Unit = {
+    val code = """
+class A {
+ def a() = {
+  if(true){
+   foo()
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(columnError(4, 10)), code, Map("TargetTokens" -> "if"))
+  }
+
+  @Test def testIfWithMultipleExprBody(): Unit = {
+    val code = """
+class A {
+ def a() = {
+  if(true){
+   foo()
+   bar()
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "if"))
+  }
+
+  @Test def testElseOK(): Unit = {
+    val code = """
+class A {
+ def a() = {
+  if(true){
+   foo()
+  }else bar()
+ }
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "else"))
+  }
+
+  @Test def testElseWithSingleExprBody(): Unit = {
+    val code = """
+class A {
+ def a() = {
+  if(true){
+   foo()
+  }else{
+   bar()
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(columnError(6, 7)), code, Map("TargetTokens" -> "else"))
+  }
+
+  @Test def testElseWithMultipleExprBody(): Unit = {
+    val code = """
+class A {
+ def a() = {
+  if(true){
+   foo()
+  }else{
+   bar()
+   bar()
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "else"))
+  }
+
+  @Test def testCaseWithSingleExprBody(): Unit = {
+    val code = """
+class A {
+ def a() = 1 match {
+  case x => Nil
+  case y => {
+   Nil
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(columnError(5, 12)), code, Map("TargetTokens" -> "case"))
+  }
+
+  @Test def testCaseWithMultipleExprBody(): Unit = {
+    val code = """
+class A {
+ def a() = 1 match {
+  case x => Nil
+  case y => {
+   foo()
+   bar()
+  }
+ }
+}
+""".stripMargin
+    assertErrors(List(), code, Map("TargetTokens" -> "case"))
+  }
+}
