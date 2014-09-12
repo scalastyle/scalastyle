53a79b2187e0e6ce1913174348430edd527f9070
Prashant Sharma <prashant.s@imaginea.com>
2014-09-12 16:52:37 +0530

    A new style checker to disallow non ascii character.

---
+++ src/test/scala/org/scalastyle/scalariform/NonASCIICharacterCheckerTest.scala
@@ -0,0 +1,53 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scalastyle.scalariform
+
+import org.junit.Test
+import org.scalastyle.file.CheckerTest
+import org.scalatest.junit.AssertionsForJUnit
+
+class NonASCIICharacterCheckerTest extends AssertionsForJUnit with CheckerTest {
+  override protected val key: String = "non.ascii.character.disallowed"
+  override protected val classUnderTest = classOf[NonASCIICharacterChecker]
+
+
+  @Test def testClassOK(): Unit = {
+    val source = """
+package foobar
+
+class OK {
+  def -> = "something"
+  val `=>` = "test"
+}
+                 """;
+
+    assertErrors(List(), source)
+  }
+
+
+  @Test def testClassNotOk(): Unit = {
+    val source = """
+package foobar
+
+class NotOK {
+  def → = "something"
+  def `⇒` = "test"
+}
+                 """;
+    assertErrors(List(columnError(5, 6), columnError(6, 6)), source)
+  }
+}
