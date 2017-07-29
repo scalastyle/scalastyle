c7cc0ecea1f30e84e1ba11d8c5f9c17b43cb0bfe
Matthew Farwell <matthew@farwell.co.uk>
2017-07-30 00:25:30 +0200

    Added tests to ensure example xml is more correct

--- src/test/scala/org/scalastyle/ScalastyleDefinitionTest.scala
+++ src/test/scala/org/scalastyle/ScalastyleDefinitionTest.scala
@@ -24,6 +24,7 @@ import org.junit.Test
 import org.scalatest.junit.AssertionsForJUnit
 
 import scala.collection.convert.WrapAsScala.asScalaBuffer
+import scala.xml.Elem
 import scala.xml.NodeSeq
 import scala.xml.XML
 
@@ -32,20 +33,20 @@ import scala.xml.XML
 class ScalastyleDefinitionTest extends AssertionsForJUnit {
 
   private def isChecker(ci: ClassPath.ClassInfo) = {
-    ci.getName().startsWith("org.scalastyle") && {
+    ci.getName.startsWith("org.scalastyle") && {
       val clazz = ci.load
 
-      classOf[Checker[_]].isAssignableFrom(clazz) && !Modifier.isAbstract(clazz.getModifiers())
+      classOf[Checker[_]].isAssignableFrom(clazz) && !Modifier.isAbstract(clazz.getModifiers)
     }
   }
 
   @Test
   def checkAllCheckersInScalastyleDefinition(): Unit = {
-    val classLoader = this.getClass().getClassLoader()
+    val classLoader = this.getClass.getClassLoader
     val config = ConfigFactory.load()
 
-    val cp = ClassPath.from(this.getClass().getClassLoader())
-    val subTypes = asScalaBuffer(cp.getAllClasses().asList()).filter(isChecker)
+    val cp = ClassPath.from(this.getClass.getClassLoader)
+    val subTypes = asScalaBuffer(cp.getAllClasses.asList()).filter(isChecker)
     val definition = ScalastyleDefinition.readFromXml(classLoader.getResourceAsStream("scalastyle_definition.xml"))
     val messageHelper = new MessageHelper(config)
 
@@ -55,7 +56,9 @@ class ScalastyleDefinitionTest extends AssertionsForJUnit {
 
     assert(missing.isEmpty, "scalastyle_definition does not contain " + missing)
 
-    val scalastyleDocumentation = (XML.load(this.getClass.getClassLoader.getResource("scalastyle_documentation.xml")) \\ "check").map { ns =>
+    val docXml = XML.load(this.getClass.getClassLoader.getResource("scalastyle_documentation.xml"))
+
+    val scalastyleDocumentation: Map[String, Documentation] = (docXml \\ "check").map { ns =>
       val doc = Documentation(toText(ns \\ "justification"), toText(ns \\ "extra-description"), toList(ns \\ "example-configuration"))
       (ns.attribute("id").get.text, doc)
     }.toMap
@@ -63,19 +66,34 @@ class ScalastyleDefinitionTest extends AssertionsForJUnit {
     checkers.foreach { c =>
       val checker = Class.forName(c.className).newInstance().asInstanceOf[Checker[_]]
 
-      val m = checker.getClass().getMethod("errorKey")
+      val m = checker.getClass.getMethod("errorKey")
       val errorKey = m.invoke(checker)
 
       assert(errorKey == c.id, "errorKey and id do not match for " + c.className)
 
       assert(scalastyleDocumentation.get(c.id).isDefined, "errorKey and id does not have documentation")
-      assert(scalastyleDocumentation.get(c.id).get.justification.size > 0, c.id + ": justification not defined")
-      assert(scalastyleDocumentation.get(c.id).get.example.size > 0, c.id + ": example not defined")
+      assert(scalastyleDocumentation(c.id).justification.isDefined, c.id + ": justification not defined")
+      assert(scalastyleDocumentation(c.id).example.nonEmpty, c.id + ": example not defined")
+
+      scalastyleDocumentation(c.id).example.foreach { e =>
+        val ns: Elem = scala.xml.XML.loadString(e)
+
+        assert(ns.attribute("class").map(_.text) == Some(c.className))
+        assert(ns.attribute("enabled").map(_.text) == Some("true"))
+        assert(ns.attribute("level").map(_.text) == Some("warning"))
+
+        val params = ns \\ "parameters"
+        if (params.nonEmpty) {
+          val list = ns \\ "parameters" \\ "parameter"
+          assert (list.size > 0, "if we have parameters, then we should have a list of parameter")
+        }
+        println("params=" + params)
+      }
     }
   }
 
   private def toText(elem: NodeSeq) = if (elem.isEmpty) None else Some(elem.text.trim)
   private def toList(elem: NodeSeq) = elem.map(_.text.trim)
 
-  case class Documentation(val justification: Option[String], val extraDescription: Option[String], val example: Seq[String])
+  case class Documentation(justification: Option[String], extraDescription: Option[String], example: Seq[String])
 }
