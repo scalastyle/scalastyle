d08547669ebe0a6b45ac3217dc803e8a933986d6
Matthew Farwell <matthew@farwell.co.uk>
2019-01-06 19:51:38 +0100

    Implemented NamedArgumentChecker

--- src/main/scala/org/scalastyle/scalariform/NamedArgumentChecker.scala
+++ src/main/scala/org/scalastyle/scalariform/NamedArgumentChecker.scala
@@ -18,52 +18,42 @@ package org.scalastyle.scalariform
 
 import org.scalastyle._
 
-import _root_.scalariform.lexer.{ Token, Tokens }
-import _root_.scalariform.parser._
+import scala.meta.Lit
+import scala.meta.Term
 import scala.util.matching.Regex
 
 /**
- * Checks method calls to ensure that passed literals are named.
- */
-class NamedArgumentChecker extends ScalariformChecker {
+  * Checks method calls to ensure that passed literals are named.
+  */
+class NamedArgumentChecker extends CombinedMetaChecker {
   protected val errorKey: String = "named.argument"
   val DefaultCheckString = false
   val DefaultIgnoreMethod = "^set.+$"
 
-  override def verify(ast: CompilationUnit): List[ScalastyleError] = {
+  override def verify(ast: CombinedMeta): List[ScalastyleError] = {
     val checkString = getBoolean("checkString", DefaultCheckString)
     val ignoreMethod = getString("ignoreMethod", DefaultIgnoreMethod)
 
-    localVisit(checkString, ignoreMethod.r)(ast.immediateChildren.head)
+    for {
+      a <- SmVisitor.getAll[Term.Apply](ast.tree)
+      arg <- matchingArguments(a, checkString, ignoreMethod.r)
+    } yield {
+      toError(arg)
+    }
   }
 
-  private def localVisit(checkString: Boolean, ignoreMethod: Regex)(ast: Any): List[ScalastyleError] = ast match {
-    case CallExpr(_, Token(_, ignoreMethod(), _, _), _, _, _) =>
+  private def matchingArguments(a: Term.Apply, checkString: Boolean, ignoreMethod: Regex): List[Term] = {
+    if (ignoreMethod.findFirstIn(a.fun.toString).isDefined) {
       Nil
+    } else {
+      a.args.filter(argMatches(checkString))
+    }
+  }
 
-    case Argument(Expr(List(GeneralTokens(List(Token(tokenType, _, offset, _)))))) =>
-      val namelessLiteral = tokenType match {
-        case Tokens.NULL |
-             Tokens.FALSE | Tokens.TRUE |
-             Tokens.INTEGER_LITERAL |
-             Tokens.CHARACTER_LITERAL |
-             Tokens.FLOATING_POINT_LITERAL =>
-          true
-        case Tokens.STRING_LITERAL =>
-          checkString
-        case _ =>
-          false
-      }
-      if (namelessLiteral) {
-        List(PositionError(offset))
-      } else {
-        Nil
-      }
-
-    case Argument(Expr(List(StringInterpolation(Token(_, _, offset, _), _, _)))) if checkString =>
-      List(PositionError(offset))
-
-    case t =>
-      VisitorHelper.visit(t, localVisit(checkString, ignoreMethod))
+  private def argMatches(checkString: Boolean)(term: Term): Boolean = term match {
+    case Term.Assign(lhs: Term.Name, _) => false
+    case l: Lit                         => true
+    case x: Term.Interpolate            => checkString
+    case _                              => false
   }
 }
--- src/test/scala/org/scalastyle/scalariform/NamedArgumentCheckerTest.scala
+++ src/test/scala/org/scalastyle/scalariform/NamedArgumentCheckerTest.scala
@@ -1,62 +1,125 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.scalastyle.scalariform
 
 import org.junit.Test
 import org.scalastyle.file.CheckerTest
 import org.scalatest.junit.AssertionsForJUnit
 
+// scalastyle:off magic.number
+
 class NamedArgumentCheckerTest extends AssertionsForJUnit with CheckerTest {
   val key = "named.argument"
   val classUnderTest = classOf[NamedArgumentChecker]
 
-  @Test def testDefault(): Unit = {
-    val source = """b(b = true)
+  @Test def testDefaultOK(): Unit = {
+    val source = """
+                   |package foobar
+                   |
+                   |class Foobar {
+                   |b(b = true)
                    |i(i = 1)
                    |l(l = 1L)
                    |f(f = 1.2)
                    |c(c = 'a')
                    |n(n = null)
+                   |h(x = foobar())
+                   |}
                  """.stripMargin
-    val badSource = """b(true)
+
+    assertErrors(List.empty, source)
+  }
+
+  @Test def testDefaultKO(): Unit = {
+    val source = """
+                      |package foobar
+                      |
+                      |class Foobar {
+                      |b(true)
                       |i(1)
                       |l(1L)
                       |f(1.2)
                       |c('a')
                       |n(null)
+                      |h(foobar())
+                      |}
                     """.stripMargin
 
-    assertErrors(List.empty, source)
     assertErrors(List(
-      columnError(1, 2),
-      columnError(2, 2),
-      columnError(3, 2),
-      columnError(4, 2),
       columnError(5, 2),
-      columnError(6, 2)), badSource)
+      columnError(6, 2),
+      columnError(7, 2),
+      columnError(8, 2),
+      columnError(9, 2),
+      columnError(10, 2)), source)
   }
 
-  @Test def testString(): Unit = {
-    val source = """s1(s = "abc")
+  @Test def testStringOK(): Unit = {
+    val source = """
+                   |package foobar
+                   |
+                   |class Foobar {
+                   |s1(s = "abc")
                    |s2(s = s"a$bc")
+                   |}
                  """.stripMargin
-    val badSource = """s1("abc")
-                      |s2(s"a${b}c")
-                    """.stripMargin
 
     assertErrors(List.empty, source, Map("checkString" -> "true"))
-    assertErrors(List(
-      columnError(1, 3),
-      columnError(2, 3)), badSource, Map("checkString" -> "true"))
   }
 
-  @Test def testIgnore(): Unit = {
-    val source1 = """setF(1)"""
-    val source2 = """f(1)"""
+  @Test def testStringKO(): Unit = {
+    val source = """
+                      |package foobar
+                      |
+                      |class Foobar {
+                      |s1("abc")
+                      |s2(s"a${b}c")
+                      |}
+                    """.stripMargin
 
-    assertErrors(List.empty, source1)
-    assertErrors(List(columnError(1, 2)), source2)
+    assertErrors(List(
+      columnError(5, 3),
+      columnError(6, 3)), source, Map("checkString" -> "true"))
+  }
 
-    assertErrors(List(columnError(1, 5)), source1, Map("ignoreMethod" -> "^f$"))
-    assertErrors(List.empty, source2, Map("ignoreMethod" -> "^f$"))
+  @Test def testIgnoreOK1(): Unit = {
+    val source = """package foobar
+                    |
+                    |class Foobar {
+                    |setF(1)
+                    |}
+                    |""".stripMargin
+
+    assertErrors(List.empty, source)
+
+    assertErrors(List(columnError(4, 5)), source, Map("ignoreMethod" -> "^f$"))
+  }
+
+  @Test def testIgnoreOK2(): Unit = {
+    val source = """package foobar
+                    |
+                    |class Foobar {
+                    |f(1)
+                    |}
+                    |""".stripMargin
+
+    assertErrors(List(columnError(4, 2)), source)
+
+    assertErrors(List.empty, source, Map("ignoreMethod" -> "^f$"))
   }
 
 }
